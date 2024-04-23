9faabf0ed186d98101ffb4c04a7e9087bf6cfe71
DhivaRunkat <dhivarunkat026@student.unsrat.ac.id>
2024-04-23 17:20:38 +0800

    new

---
+++ script.js
@@ -0,0 +1,14 @@
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function(event) {
+    const targetSection = document.getElementById(this.getAttribute('href').substring(1));
+    if (targetSection) {
+      event.preventDefault();
+      targetSection.scrollIntoView({
+        behavior: 'smooth', 
+        block: 'start' 
+      });
+    }
+  });
+});
